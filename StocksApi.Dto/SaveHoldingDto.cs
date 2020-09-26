65f849c6cac38d9b368c13ca3b2f700f53a3831c
David Fidge <dfidge@internode.on.net>
2020-09-26 20:34:17 +0930

    add required attributes to save holdings dto

--- StocksApi.Dto/SaveHoldingDto.cs
+++ StocksApi.Dto/SaveHoldingDto.cs
@@ -1,14 +1,25 @@
 ﻿using System;
+using System.ComponentModel.DataAnnotations;
 
 namespace StocksApi.Controllers
 {
     public class SaveHoldingDto : BaseDto
     {
+        [Required]
         public string StockCode { get; set; }
+
+        [Required]
         public DateTime PurchaseDate { get; set; }
+
+        [Required]
         public long NumberOfShares { get; set; }
+
+        [Required]
         public Decimal PurchasePrice { get; set; }
+
+        [Required]
         public Decimal Brokerage { get; set; }
+
         public Guid PortfolioId { get; set; }
     }
 }
